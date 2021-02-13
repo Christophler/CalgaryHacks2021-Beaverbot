554cd9f9b1e104bf79b1120346bd543c0711f786
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-02-13 17:18:27 -0500

    playing around with sql

--- src/main/sql/sql_test.js
+++ src/main/sql/sql_test.js
@@ -1,12 +1,18 @@
-const { query } = require('faunadb');
-var faunadb = require('faunadb');
+var {query, faunadb} = require('faunadb');
 var q = query;
 var client = new faunadb.Client({ secret: 'fnAEB9705sACAlJREYUIpnkqRK-W5SNwPlmXhl4b' });
 
+const name = "jeremy#7970"
+
+const data = {
+    title: "hello world",
+    description: "uiniversity of toronto kms"
+}
+
 client.query(
-    q.Create(
+    q.Create (
         q.Collection('profiles'),
-        {data: {title: "Hello there"}},
+        {data: data},
     )
 )
 .then((ret) => {
