ec330e3c25eb037e1859abaff416cd1825c95a69
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-02-13 14:20:14 -0500

    woohoo

--- src/main/index.js
+++ src/main/index.js
@@ -1,14 +1,17 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-bot.once('ready', () =>{
+bot.once('ready', () => {
     console.log('Ready!');
 });
+
+console.log("askldjkasd");
 
 bot.login('ODEwMjI1MzMwMjgyOTU0Nzky.YCgjIw.k2u4fW0dBIk8CkpbQYZKMgXA5Oc');
 
 bot.once("message", (msg) => {
     console.log(msg);
+    // hello there
 });
 
 /*
