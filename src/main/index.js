0e7b1381b6789f876f3621741baf795e3dc45924
Henry <44884792+hoomji@users.noreply.github.com>
2021-02-13 14:12:18 -0500

    main code
    main code

---
+++ src/main/index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.once('ready', () =>{
+    console.log('Ready!');
+});
+
+bot.login('ODEwMjI1MzMwMjgyOTU0Nzky.YCgjIw.k2u4fW0dBIk8CkpbQYZKMgXA5Oc');
+
+bot.once("message", (msg) => {
+    console.log(msg);
+});
+
+/*
+const my_array = [1,2,'jermey'];
+console.log(my_array);
+
+
+
+const my_function = (message) => {
+
+}*/
