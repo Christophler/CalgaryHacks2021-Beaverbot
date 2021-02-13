4965767575f3d3eebffeedbae52728616325c1c4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-02-13 16:27:00 -0500

    non-working sql stuff

--- src/main/sql/sql_test.js
+++ src/main/sql/sql_test.js
@@ -1,9 +1,14 @@
-var faunadb = require('faunadb'), q = faunadb.query;
-var client = new faunadb.Client({ secret: 'fnAEB9705sACAlJREYUIpnkqRK-W5SNwPlmXhl4b' })
+const { query } = require('faunadb');
+var faunadb = require('faunadb');
+var q = query;
+var client = new faunadb.Client({ secret: 'fnAEB9705sACAlJREYUIpnkqRK-W5SNwPlmXhl4b' });
 
-var createP = client.query(
+client.query(
     q.Create(
-      q.Collection('test'),
-      { data: { testField: 'testValue' } }
+        q.Collection('profiles'),
+        {data: {title: "Hello there"}},
     )
-  )
+)
+.then((ret) => {
+    console.log(ret);
+})
