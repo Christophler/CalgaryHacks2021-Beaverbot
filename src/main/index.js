0769518bea9cddb30433401f6ec9bfccc01664de
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-02-13 15:37:11 -0500

    more changes

--- src/main/index.js
+++ src/main/index.js
@@ -10,11 +10,34 @@ bot.on("message", (message) => {
     if(message.author.bot) return; // terminate if the message sent was from the bot.
 
     const embed = {
-        "title": "Title",
-        "description": "Jeremy was here",
+        "title": "University of Toront",
+        "description": "Discord Server Link:",
         "color": 99999,
         "timestamp": "2021-02-13T20:29:35.728Z",
-        "fields": []
+        "fields": [
+            {
+                "name": "Programs",
+                "value": "Computer Science"
+              },
+              {
+                "name": "😱",
+                "value": "try exceeding some of them!"
+              },
+              {
+                "name": "🙄",
+                "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+              },
+              {
+                "name": "<:thonkang:219069250692841473>",
+                "value": "these last two",
+                "inline": true
+              },
+              {
+                "name": "<:thonkang:219069250692841473>",
+                "value": "are inline fields",
+                "inline": true
+              }
+        ]
     };
 
     message.channel.send({embed});
