e6355150c313e488c288dcbf2b23ea10fb453644
Henry <44884792+hoomji@users.noreply.github.com>
2021-02-13 19:48:45 -0500

    registercmd

--- src/main/commands/register_command.js
+++ src/main/commands/register_command.js
@@ -2,6 +2,7 @@
 
 const CommandBase = require('./command_base');
 const {registerProfile} = require('../sql/sql_util');
+const unis = require('DIRECTORY_TO_JSON').universities;
 
 class RegisterCommand extends CommandBase {
 
@@ -16,14 +17,18 @@ class RegisterCommand extends CommandBase {
         // Usage: !register <first_name> <last_name> <uni_tag>
         // Example: !register Jeremy Tubongbanua uoft
 
-        const first = args[0];
-        const last = args[1];
-        const uniTag = args[2];
-        //const discordId = ? // find through Message object on discord (see Discord.js docs on Message)
+        if (args.length == 3){
 
-        // call registerProfile()
+            const first = args[0]; 
+            const last = args[1];
+            const uniTag = args[2];
+            const discordId = message.member.user.tag // find through Message object on discord (see Discord.js docs on Message)
 
+            registerProfile(discordId, first, last, uniTag);
+        }
 
     }
 
 }
+
+module.exports = RegisterCommand;
