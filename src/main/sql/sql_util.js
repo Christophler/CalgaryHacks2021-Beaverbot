199672b65561f6cf99d415600b9d4a8214dcb3b2
Christophler <69325845+Christophler@users.noreply.github.com>
2021-02-13 19:01:12 -0500

    a
    a

--- src/main/sql/sql_util.js
+++ src/main/sql/sql_util.js
@@ -2,6 +2,21 @@ var {query, faunadb} = require('faunadb');
 var q = query;
 var client = new faunadb.Client({ secret: 'fnAEB9705sACAlJREYUIpnkqRK-W5SNwPlmXhl4b' });
 
+
+function registerUniversity(universityName, tag2) {
+    const data = {
+        name: universityName,
+        tag: tag2,
+        programs: [
+            "Agriculture & Natural Resources Conservation", "Architecture", "Area, Ethnic & Multidisciplinary Studies", 
+            "Arts: Visual & Performing", "Business", "Communications", "Community, Family & Personal Services", "Computer Science & Mathematics", 
+            "Education", "Engineering", "English & Foreign Languages", "Health Sciences & Technologies", "Philosophy, Religion & Theology", 
+            "Repair, Production & Construction", "Sciencess: Biological & Physical", "Social Sciences & Law"
+        ]
+    }
+}
+
+
 client.query(
     q.Create (
         q.Collection('profiles'),
@@ -12,8 +27,3 @@ client.query(
     console.log(ret);
 })
 
-function registerUniversity(universityName, tag2) {
-    var name = universityName;
-    var tag = tag2;
-    
-}
